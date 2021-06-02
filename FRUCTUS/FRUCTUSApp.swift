992a6946f2ef52ea528017389656f0e58ed1da71
JJ Santos <jj@deltaprotect.co>
2021-06-02 12:34:40 -0500

    Initial Commit

---
+++ FRUCTUS/FRUCTUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUCTUSApp.swift
+//  FRUCTUS
+//
+//  Created by JJ Santos on 02/06/21.
+//
+
+import SwiftUI
+
+@main
+struct FRUCTUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
