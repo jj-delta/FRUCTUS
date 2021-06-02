02557e27bd2c247b2e31c5886a89c35c04acdb25
JJ Santos <jj@deltaprotect.co>
2021-06-02 14:04:30 -0500

    Fruit Model + Fruit View

---
+++ FRUCTUS/App/OnboardingView.swift
@@ -0,0 +1,33 @@
+//
+//  OnboardingView.swift
+//  FRUCTUS
+//
+//  Created by JJ Santos on 02/06/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { item in
+                FruitCardView()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
